7af8892c0147c279243ca7fabe5117a241c591c6
VidushaSanidu <vidushsanidu07@gmail.com>
2024-09-24 10:02:50 +0530

    minor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Post('student/register')
-  async register(@Body() createUserDto: CreateUserDto) {
-    return this.authService.register(createUserDto);
-  }
+  // @Post('student/register')
+  // async register(@Body() createUserDto: CreateUserDto) {
+  //   return this.authService.register(createUserDto);
+  // }
 }
