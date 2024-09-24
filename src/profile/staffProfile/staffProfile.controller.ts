9e193f9232a369264b75cc4c6dab234b4c1c097a
Dilshan <tharakadilshan506@gmail.com>
2024-09-24 12:54:50 +0530

    staffProfile Controller

---
+++ src/profile/staffProfile/staffProfile.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { StaffProfileService } from './staffProfile.service';
+import { CreateStaffProfileDto } from './dto/create-staffProfile.dto';
+import { UpdateStaffProfileDto } from './dto/update-staffProfile.dto';
+  
+@Controller('staff-profile')
+export class StaffProfileController {
+    constructor(private readonly staffProfileService: StaffProfileService) {}
+  
+    @Get()
+    findAll() {
+      return this.staffProfileService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+      return this.staffProfileService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() createStaffProfileDto: CreateStaffProfileDto) {
+      return this.staffProfileService.create(createStaffProfileDto);
+    }
+  
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateStaffProfileDto: UpdateStaffProfileDto) {
+      return this.staffProfileService.update(id, updateStaffProfileDto);
+    }
+  
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+      return this.staffProfileService.remove(id);
+    }
+}
