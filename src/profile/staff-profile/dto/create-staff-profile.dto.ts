ccdbd39f6be5a0f7764b9b75ae81c05913a88dd2
VidushaSanidu <vidushsanidu07@gmail.com>
2024-09-25 18:40:08 +0530

    dto bug fixef

--- src/profile/staff-profile/dto/create-staff-profile.dto.ts
+++ src/profile/staff-profile/dto/create-staff-profile.dto.ts
@@ -1,27 +1,36 @@
-import { IsString, IsOptional, IsArray, IsEmail, MaxLength, Length } from 'class-validator';
-  
+import {
+  IsString,
+  IsArray,
+  IsEmail,
+  MaxLength,
+  Length,
+  IsEnum,
+  ArrayMaxSize,
+} from 'class-validator';
+import { Title } from 'enums/title.enum';
+
 export class CreateStaffProfileDto {
-    @IsString()
-    @MaxLength(50)
-    displayName: string;
-  
-    @IsString()
-    designation: string;
-  
-    @IsString()
-    nominal: string;
-  
-    @IsString()
-    @MaxLength(10)
-    extNo?: string;
-  
-    @IsArray()
-    @IsEmail({}, { each: true })
-    @MaxLength(3)
-    emails: string[];
-  
-    @IsArray()
-    @Length(7, 15, { each: true }) // Assuming telephone numbers are between 7 and 15 digits
-    @MaxLength(3)
-    telephones: string[];
+  @IsEnum(Title)
+  designation: Title;
+
+  @IsString()
+  @MaxLength(50)
+  displayName: string;
+
+  @IsString()
+  nominal: string;
+
+  @IsString()
+  @MaxLength(10)
+  extNo?: string;
+
+  @IsArray()
+  @IsEmail({}, { each: true })
+  @MaxLength(2)
+  emails: string[];
+
+  @IsArray()
+  @Length(7, 15, { each: true }) // telephone numbers are between 7 and 15 digits
+  @ArrayMaxSize(3)
+  telephones: string[];
 }
