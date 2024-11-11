68dbde2f924b659b25cf670b4139e5ebdc2a1de3
VidushaSanidu <vidushsanidu07@gmail.com>
2024-11-12 00:58:40 +0530

    update reserve record services

--- src/reserve-records/reserve-records.service.ts
+++ src/reserve-records/reserve-records.service.ts
@@ -1,26 +1,62 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Not, Repository, UpdateResult } from 'typeorm';
 import { CreateReserveRecordDto } from './dto/create-reserve-record.dto';
 import { UpdateReserveRecordDto } from './dto/update-reserve-record.dto';
+import { ReserveRecord } from './entities/reserve-record.entity';
 
 @Injectable()
 export class ReserveRecordsService {
-  create(createReserveRecordDto: CreateReserveRecordDto) {
-    return 'This action adds a new reserveRecord';
+  constructor(
+    @InjectRepository(ReserveRecord)
+    private reserveRecordRepository: Repository<ReserveRecord>,
+  ) {}
+
+  async create(
+    createReserveRecordDto: CreateReserveRecordDto,
+  ): Promise<ReserveRecord> {
+    const reserveRecord = this.reserveRecordRepository.create(
+      createReserveRecordDto,
+    );
+    return this.reserveRecordRepository.save(reserveRecord);
   }
 
-  findAll() {
-    return `This action returns all reserveRecords`;
+  async findAll(): Promise<ReserveRecord[]> {
+    return this.reserveRecordRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} reserveRecord`;
+  // get all pending records
+  async findAllPending(): Promise<ReserveRecord[]> {
+    return this.reserveRecordRepository.find({ where: { status: 'Pending' } });
   }
 
-  update(id: number, updateReserveRecordDto: UpdateReserveRecordDto) {
-    return `This action updates a #${id} reserveRecord`;
+  // get all records that not ended
+  async findAllNotEnded(): Promise<ReserveRecord[]> {
+    return this.reserveRecordRepository.find({
+      where: { status: Not('Done') },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} reserveRecord`;
+  async findOne(id: string): Promise<ReserveRecord> {
+    return this.reserveRecordRepository.findOne({ where: { id } });
+  }
+
+  async update(
+    id: string,
+    updateReserveRecordDto: UpdateReserveRecordDto,
+  ): Promise<UpdateResult> {
+    if (this.reserveRecordRepository.findOne({ where: { id } })) {
+      throw new Error('Record not found');
+    }
+
+    const newRecord = await this.reserveRecordRepository.update(
+      id,
+      updateReserveRecordDto,
+    );
+    return newRecord;
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.reserveRecordRepository.delete(id);
   }
 }
