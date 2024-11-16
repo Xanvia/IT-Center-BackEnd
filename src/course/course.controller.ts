844c741f8acde3ec499e9cf7d52e8be7b793cc47
Dilshan <tharakadilshan506@gmail.com>
2024-11-16 21:46:09 +0530

    create course-countroller class

---
+++ src/course/course.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Post, Get, Body, Param, Patch } from '@nestjs/common';
+import { CourseService } from './course.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { EnrollCourseDto } from './dto/enroll-course.dto';
+
+@Controller('course')
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Post()
+  createCourse(@Body() createCourseDto: CreateCourseDto) {
+    return this.courseService.createCourse(createCourseDto);
+  }
+
+  @Get()
+  getAllCourses() {
+    return this.courseService.getAllCourses();
+  }
+
+  @Post('enroll')
+  enrollInCourse(@Body() enrollCourseDto: EnrollCourseDto) {
+    return this.courseService.enrollInCourse(enrollCourseDto);
+  }
+
+  @Patch('enrollment/:id/approve')
+  approveEnrollment(@Param('id') id: string) {
+    return this.courseService.approveEnrollment(id);
+  }
+}
