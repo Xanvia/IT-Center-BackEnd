f9c8d1d2150c9cac875a2ecf0be5369b33e56ab5
VidushaSanidu <vidushsanidu07@gmail.com>
2025-06-17 13:52:08 +0530

    change

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -13,7 +16,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:5000', // Frontend URL
+    origin: configService.get<string>('CORS_ORIGIN'),
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
@@ -22,6 +25,7 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
 
-  await app.listen(5100);
+  const port = configService.get<number>('PORT') || 5100;
+  await app.listen(port);
 }
 bootstrap();
